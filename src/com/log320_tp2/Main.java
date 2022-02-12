2743be85c79a9c83f3b5777f15797e5db2204e76
David Daniel <davide018@hotmail.com>
2022-02-12 18:09:59 -0500

    d

--- src/com/log320_tp2/Main.java
+++ src/com/log320_tp2/Main.java
@@ -5,7 +5,7 @@ public class Main
     public static void main(String[] args) throws Exception {
         Huffman huffman = new Huffman();
 
-        huffman.Compresser("C:\\Users\\david\\Downloads\\exemple.txt",
+        huffman.Compresser("C:\\Users\\david\\Downloads\\exemple.tx",
                 "C:\\Users\\david\\OneDrive\\Documents\\Compressed.txt");
     }
 }
